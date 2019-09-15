82ee9ba5ba895cd17f1cc6ee104e1cb0df572fd3
joanntharian96 <jtharian@iu.edu>
2019-09-15 19:14:00 -0400

    updated  javascript file to handle bind error

--- app/public/js/waiting.app.js
+++ app/public/js/waiting.app.js
@@ -1,8 +1,60 @@
 var waitingApp = new Vue({
   el: '#personWaitingApp',
   data: {
-    person:{}
-},
+    person:{
+      "gender": "",
+      "name": {
+       "title": "",
+       "first": "",
+       "last": ""
+     },
+     "location": {
+        "street": "",
+        "city": "",
+        "state": "",
+        "postcode": "",
+        "coordinates": {
+          "latitude": "",
+          "longitude": ""
+        },
+        "timezone": {
+          "offset": "",
+          "description": ""
+        }
+      },
+      "email": "",
+      "login": {
+        "uuid": "",
+        "username": "",
+        "password": "",
+        "salt": "",
+        "md5": "",
+        "sha1": "",
+        "sha256": ""
+      },
+      "dob": {
+        "date": "",
+        "age": ""
+      },
+      "registered": {
+        "date": "",
+        "age": ""
+      },
+      "phone": "",
+      "cell": "",
+      "id": {
+        "name": "",
+        "value": ""
+      },
+      "picture": {
+        "large": "",
+        "medium": "",
+        "thumbnail": ""
+      },
+      "nat": ""
+    }
+  },
+
 
   methods:{
     fetchPerson() {

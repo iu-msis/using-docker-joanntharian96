09a165fcb3921f098c4651ce930e7ee26d744949
joanntharian96 <jtharian@iu.edu>
2019-09-21 20:20:26 -0400

    Added button on index page to refresh data

--- app/public/js/waiting.app.js
+++ app/public/js/waiting.app.js
@@ -61,11 +61,68 @@ var waitingApp = new Vue({
       fetch('https://randomuser.me/api/')
       .then(response => response.json())
       .then(randPerson=>{waitingApp.person= randPerson.results[0]});
+    },
+    newRecord(){
+      this.fetchPerson();
+      this.person={
+        "gender": "",
+        "name": {
+         "title": "",
+         "first": "",
+         "last": ""
+       },
+       "location": {
+          "street": "",
+          "city": "",
+          "state": "",
+          "postcode": "",
+          "coordinates": {
+            "latitude": "",
+            "longitude": ""
+          },
+          "timezone": {
+            "offset": "",
+            "description": ""
+          }
+        },
+        "email": "",
+        "login": {
+          "uuid": "",
+          "username": "",
+          "password": "",
+          "salt": "",
+          "md5": "",
+          "sha1": "",
+          "sha256": ""
+        },
+        "dob": {
+          "date": "",
+          "age": ""
+        },
+        "registered": {
+          "date": "",
+          "age": ""
+        },
+        "phone": "",
+        "cell": "",
+        "id": {
+          "name": "",
+          "value": ""
+        },
+        "picture": {
+          "large": "",
+          "medium": "",
+          "thumbnail": ""
+        },
+        "nat": ""
+      }
+
     }
   },
 
   created() {
     this.fetchPerson();
+
   }
 
 });
